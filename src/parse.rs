35e918a107453d4c88b3c542bc1bff5abaf0e866
Robert Gardner <rhg259@nyu.edu>
2015-11-02 15:46:39 -0500

    Add tests for single arg, multi arg

--- src/parse.rs
+++ src/parse.rs
@@ -12,3 +12,39 @@ pub fn parse(input: &str) -> Option<Command> {
         Some(cmd)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::process::{Command, Output};
+
+    fn echo_nothing() -> Output {
+        Command::new("echo").output().unwrap()
+    }
+
+    fn echo_hello() -> Output {
+        Command::new("echo").arg("hello").output().unwrap()
+    }
+
+    fn echo_multi() -> Output {
+        Command::new("echo").args(&vec!["hello", "brave", "new", "world"]).output().unwrap()
+    }
+
+    #[test]
+    fn single_cmd() {
+        let mut cmd = parse("echo").unwrap();
+        assert_eq!(echo_nothing().stdout, cmd.output().unwrap().stdout);
+    }
+
+    #[test]
+    fn single_cmd_with_args() {
+        let mut cmd = parse("echo hello brave new world").unwrap();
+        assert_eq!(echo_multi().stdout, cmd.output().unwrap().stdout);
+    }
+
+    #[test]
+    fn single_cmd_with_arg() {
+        let mut cmd = parse("echo hello").unwrap();
+        assert_eq!(echo_hello().stdout, cmd.output().unwrap().stdout);
+    }
+}
