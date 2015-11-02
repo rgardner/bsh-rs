f7e39d25001920da6dc4a53dfdbf1c27c5ebe04b
Robert Gardner <rhg259@nyu.edu>
2015-11-01 21:46:54 -0500

    Add editorconfig, rustfmt, minimal echo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let mut input = String::new();
+
+        io::stdin().read_line(&mut input)
+            .ok()
+            .expect("failed to read line");
+
+        println!("No, you're a {}", input);
+    }
 }
