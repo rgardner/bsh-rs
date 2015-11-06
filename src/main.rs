178baa00149c85af963e4cc4ab0010ce6ae7bc06
Robert Gardner <rhg259@nyu.edu>
2015-11-06 17:53:56 -0500

    Implement infile and add EOF support in main

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,34 @@
 extern crate bsh_rs;
 
-use bsh_rs::parse;
+use bsh_rs::parse::ParseInfo;
 use std::io::{self, Write};
+use std::process;
 
-fn prompt(buf: &mut String) {
+fn prompt(buf: &mut String) -> io::Result<usize> {
     print!("$ ");
     io::stdout().flush().unwrap();
-    io::stdin().read_line(buf).ok().expect("failed to read line");
+    io::stdin().read_line(buf)
 }
 
 fn main() {
     loop {
         let mut input = String::new();
-        prompt(&mut input);
-        let command = parse::parse(&input);
+        if let Ok(n) = prompt(&mut input) {
+            if n == 0 {
+                println!("");
+                process::exit(0);
+            }
+        } else {
+            panic!("failed to read line.");
+        }
+        let command = match ParseInfo::parse(&input) {
+            Ok(Some(info)) => info,
+            Err(err) => {
+                println!("{:?}", err);
+                continue
+            }
+            _ => continue,
+        };
         println!("parsed command: {:?}", command);
     }
 }
--- src/parse.rs
+++ src/parse.rs
@@ -1,6 +1,7 @@
 //! BSH Parser
 
 use std::result;
+use std::process::Command;
 
 /// A specialized Result type for Parse operations.
 ///
@@ -22,106 +23,161 @@ quick_error! {
     }
 }
 
-/// Represents a command and its associated variable parameters
-#[derive(Debug)]
-pub struct ParseCommand {
-    command: String,
-    variables: Vec<String>,
-}
-
-impl ParseCommand {
-    fn new() -> ParseCommand {
-        ParseCommand { command: String::new(), variables: Vec::new() }
-    }
-}
-
 /// Represents all information associated with a user input
 #[derive(Debug)]
 pub struct ParseInfo {
     infile: Option<String>,
     outfile: Option<String>,
     background: bool,
-    commands: Vec<ParseCommand>,
+    commands: Vec<Command>,
 }
 
 impl ParseInfo {
-    fn new() -> ParseInfo {
-        ParseInfo {
-            infile: None,
-            outfile: None,
-            background: false,
-            commands: Vec::new()
+    /// Parses input string into ParseInfo
+    pub fn parse(input: &str) -> Result<Option<ParseInfo>> {
+        let argv: Vec<_> = input.trim().split_whitespace().collect();
+        if argv.is_empty() {
+            return Ok(None);
         }
+
+        let mut info = ParseInfoBuilder::new();
+        let mut cmd = Command::new(argv[0]);
+
+        let mut infile = false;
+        for &arg in &argv[1..] {
+            if arg.starts_with("<") && !infile {
+                if arg.len() > 1 {
+                    info.infile(&arg[1..]);
+                } else {
+                    infile = true;
+                }
+            } else if infile {
+                info.infile(arg);
+                infile = false
+            } else {
+                cmd.arg(arg);
+            }
+        }
+        info.command(cmd);
+        Ok(Some(info.build()))
     }
 }
 
+/// Build Parse Info
+#[derive(Debug)]
+pub struct ParseInfoBuilder {
+    infile: Option<String>,
+    outfile: Option<String>,
+    background: bool,
+    commands: Vec<Command>,
+}
 
-/// Parses input string into ParseInfo
-pub fn parse(input: &str) -> Result<Option<ParseInfo>> {
-    let argv: Vec<_> = input.trim().split_whitespace().collect();
-    if argv.is_empty() {
-        return Ok(None);
-    }
-
-    let mut info = ParseInfo::new();
-    let mut cmd = ParseCommand::new();
-    cmd.command = String::from(argv[0]);
-
-    let mut infile = false;
-    for &arg in &argv[1..] {
-        if arg.starts_with("<") && !infile {
-            if arg.len() > 1 {
-                info.infile = Some(arg[1..].to_string());
-            } else {
-                infile = true;
-            }
-        } else if infile {
-            info.infile = Some(String::from(arg));
-            infile = false
-        } else {
-            cmd.variables.push(String::from(arg));
+impl ParseInfoBuilder {
+    /// Construct a new `ParseInfoBuilder` for parsing commands, with the following default
+    /// configuration:
+    ///
+    /// * No input/output redirection
+    /// * Runs in foreground
+    /// * No program or commands
+    ///
+    /// Builder methods are provided to change these defaults and otherwise configure the job.
+    pub fn new() -> ParseInfoBuilder {
+        ParseInfoBuilder {
+            infile: None,
+            outfile: None,
+            background: false,
+            commands: Vec::new(),
         }
     }
-    info.commands.push(cmd);
-    Ok(Some(info))
+
+    /// Add input redirection from the specified filename.
+    pub fn infile(&mut self, filename: &str) -> &mut ParseInfoBuilder {
+        self.infile = Some(String::from(filename));
+        self
+    }
+
+    /// Add output redirection to the specified filename.
+    pub fn outfile(&mut self, filename: &str) -> &mut ParseInfoBuilder {
+        self.outfile = Some(String::from(filename));
+        self
+    }
+
+    /// Configure job to run in the background.
+    pub fn background(&mut self, background: bool) -> &mut ParseInfoBuilder {
+        self.background = background;
+        self
+    }
+
+    /// Add a new command.
+    pub fn command(&mut self, command: Command) -> &mut ParseInfoBuilder {
+        self.commands.push(command);
+        self
+    }
+
+    /// Build the final job.
+    pub fn build(self) -> ParseInfo {
+        ParseInfo {
+            infile: self.infile,
+            outfile: self.outfile,
+            background: self.background,
+            commands: self.commands,
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::process::Command;
+
+    #[test]
+    fn empty() {
+        assert!(ParseInfo::parse("").unwrap().is_none());
+    }
 
     #[test]
     fn single_cmd() {
-        let info = parse("echo").unwrap().unwrap();
-        assert!(info.infile.is_none());
-        assert!(info.outfile.is_none());
-        assert_eq!(1, info.commands.len());
-        assert_eq!("echo", info.commands[0].command);
-        assert!(info.commands[0].variables.is_empty());
+        let info = ParseInfoBuilder::new().command(Command::new("cmd")).build();
+        assert_eq!(info, ParseInfo::parse("cmd").unwrap().unwrap());
     }
 
     #[test]
     fn single_cmd_with_args() {
-        let info = parse("echo hello brave new world").unwrap().unwrap();
-        assert!(info.infile.is_none());
-        assert!(info.outfile.is_none());
-        assert_eq!(1, info.commands.len());
-        assert_eq!("echo", info.commands[0].command);
-        assert_eq!(4, info.commands[0].variables.len());
-        assert_eq!("hello", info.commands[0].variables[0]);
-        assert_eq!("brave", info.commands[0].variables[1]);
-        assert_eq!("new", info.commands[0].variables[2]);
-        assert_eq!("world", info.commands[0].variables[3]);
+        let mut cmd = Command::new("cmd");
+        cmd.args(&vec!["var1", "var2", "var3"]);
+        let info = ParseInfoBuilder::new().command(cmd).build();
+        assert_eq!(info, ParseInfo::parse("cmd var1 var2 var3").unwrap().unwrap());
     }
 
     #[test]
     fn single_cmd_with_arg() {
-        let info = parse("echo hello").unwrap().unwrap();
-        assert!(info.infile.is_none());
-        assert!(info.outfile.is_none());
-        assert_eq!(1, info.commands.len());
-        assert_eq!("echo", info.commands[0].command);
-        assert_eq!(1, info.commands[0].variables.len());
-        assert_eq!("hello", info.commands[0].variables[0]);
+        let mut cmd = Command::new("cmd");
+        cmd.arg("var1");
+        let info = ParseInfoBuilder::new().command(cmd).build();
+        assert_eq!(info, ParseInfo::parse("cmd var1").unwrap().unwrap());
+    }
+
+    #[test]
+    fn infile_valid() {
+        let info = ParseInfoBuilder::new().command(Command::new("cmd")).infile("infile");
+        assert_eq!(info, ParseInfo::parse("cmd <infile"));
+        assert_eq!(info, ParseInfo::parse("cmd < infile"));
+    }
+
+    #[test]
+    fn infile_invalid() {
+        assert!(ParseInfo::parse("cmd <").unwrap().is_none());
+    }
+
+    #[test]
+    fn outfile_valid() {
+        let info = ParseInfoBuilder::new().command(Command::new("cmd")).outfile("outfile");
+        assert_eq!(info, ParseInfo::parse("cmd >outfile"));
+        assert_eq!(info, ParseInfo::parse("cmd < outfile"));
+    }
+
+    #[test]
+    fn outfile_invalid() {
+        assert!(ParseInfo::parse("cmd >").unwrap().is_none());
     }
 }
