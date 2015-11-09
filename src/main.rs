215dc535523318c886c21835a087129d958edde7
Robert Gardner <rhg259@nyu.edu>
2015-11-08 23:07:11 -0500

    Add infile/outfile redirection

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,10 @@
 extern crate bsh_rs;
 use bsh_rs::parse::ParseInfo;
 use std::env;
-use std::io::{self, Write};
+use std::fs::{File, OpenOptions};
+use std::io::{self, Read, Write};
 use std::path::Path;
+use std::process::{Stdio};
 use std::process;
 
 fn prompt(buf: &mut String) -> io::Result<usize> {
@@ -20,17 +22,37 @@ fn prompt(buf: &mut String) -> io::Result<usize> {
     io::stdin().read_line(buf)
 }
 
-fn execute_job(job: ParseInfo) {
-    for mut command in job.commands {
-        let output = match command.output() {
-            Ok(output) => output,
-            Err(e) => {
-                println!("bsh: {}", e);
-                continue;
-            }
-        };
-        println!("{}", String::from_utf8_lossy(&output.stdout));
+fn execute_job(job: &mut ParseInfo) -> Result<(), io::Error> {
+    let mut command = job.commands.get_mut(0).unwrap();
+    // if it's a builtin, call the builtin
+
+    if let Some(_) = job.infile {
+        command.stdin(Stdio::piped());
     }
+
+    if let Some(_) = job.outfile {
+        command.stdout(Stdio::piped());
+    }
+
+    let mut child = try!(command.spawn());
+    if let Some(ref mut stdin) = child.stdin {
+        let infile = job.infile.take().unwrap();
+        let mut f = try!(File::open(infile));
+        let mut buf: Vec<u8> = vec![];
+        try!(f.read_to_end(&mut buf));
+        try!(stdin.write_all(&buf));
+    }
+    if let Some(ref mut stdout) = child.stdout {
+        let outfile = job.outfile.take().unwrap();
+        let mut file = try!(OpenOptions::new().write(true).create(true).open(outfile));
+        let mut buf: Vec<u8> = vec![];
+        try!(stdout.read_to_end(&mut buf));
+        try!(file.write_all(&buf));
+    } else {
+        println!("{}",
+                 String::from_utf8_lossy(&child.wait_with_output().unwrap().stdout));
+    }
+    Ok(())
 }
 
 fn main() {
@@ -44,7 +66,7 @@ fn main() {
         } else {
             panic!("failed to read line.");
         }
-        let info = match ParseInfo::parse(&input) {
+        let mut info = match ParseInfo::parse(&input) {
             Ok(Some(info)) => info,
             Err(err) => {
                 println!("{:?}", err);
@@ -54,6 +76,8 @@ fn main() {
         };
         println!("parsed info: {:?}", info);
 
-        execute_job(info);
+        if let Err(e) = execute_job(&mut info) {
+            println!("bsh: {}", e);
+        }
     }
 }
