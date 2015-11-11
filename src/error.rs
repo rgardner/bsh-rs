4bcdeb940d791e56c268a1015b083f92db5e7cff
Robert Gardner <rhg259@nyu.edu>
2015-11-11 01:41:39 -0500

    Forward display, description, and cause for Errors

--- src/error.rs
+++ src/error.rs
@@ -4,11 +4,18 @@ use std::io;
 quick_error! {
     #[derive(Debug)]
     pub enum BshError {
-        /// IO Error
+        /// Wrapper around io::Error
         Io(err: io::Error) {
+            display("{}", err)
+            description(err.description())
+            cause(err)
             from()
         }
+        /// Wrapper around builtins::Error
         BuiltinError(err: builtins::Error) {
+            display("{}", err)
+            description(err.description())
+            cause(err)
             from()
         }
     }
