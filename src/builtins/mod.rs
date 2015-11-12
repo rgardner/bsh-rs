25dbb144c2eacfeda369c66075e92c8ce9014eab
Robert Gardner <rhg259@nyu.edu>
2015-11-12 12:08:49 -0500

    Implement exit and stub out history

--- src/builtins/mod.rs
+++ src/builtins/mod.rs
@@ -52,9 +52,9 @@ pub fn is_builtin(program: &str) -> bool {
 pub fn run(process: &ParseCommand) -> Result<()> {
     match &*process.program {
         CD => Cd::run(process.args.clone()),
-        EXIT => exit(process.args.clone()),
+        EXIT => Exit::run(process.args.clone()),
         HELP => Help::run(process.args.clone()),
-        HISTORY => history(process.args.clone()),
+        HISTORY => History::run(process.args.clone()),
         _ => unreachable!(),
     }
 }
@@ -82,9 +82,9 @@ help: help [pattern ...]
             for arg in &args {
                 let msg = match (*arg).as_ref() {
                     CD => Some(Cd::help()),
-                    EXIT => Some(String::new()),
+                    EXIT => Some(Exit::help()),
                     HELP => Some(Help::help()),
-                    HISTORY => Some(String::new()),
+                    HISTORY => Some(History::help()),
                     _ => None,
                 };
                 if let Some(msg) = msg {
@@ -102,22 +102,56 @@ help: help [pattern ...]
     }
 }
 
-fn exit(args: Vec<String>) -> Result<()> {
-    println!("exit");
-    if let Some(code) = args.get(0) {
-        let code: i32 = match code.parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("bsh: exit: {}: numeric argument required", code);
-                2
-            }
-        };
-        process::exit(code);
-    } else {
-        process::exit(0);
+struct Exit;
+
+impl BuiltinCommand for Exit {
+    fn name() -> String {
+        String::from("exit")
+    }
+
+    fn help() -> String {
+        String::from("\
+exit: exit [n]
+    Exit the shell with a status of N. If N is omitted, the exit status
+    is 0.")
+    }
+
+    fn run(args: Vec<String>) -> Result<()> {
+        println!("exit");
+        if let Some(code) = args.get(0) {
+            let code: i32 = match code.parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("bsh: exit: {}: numeric argument required", code);
+                    2
+                }
+            };
+            process::exit(code);
+        } else {
+            // TODO(rgardner): is that of the last command executed.")
+            process::exit(0);
+        }
     }
 }
 
-fn history(_args: Vec<String>) -> Result<()> {
-    Ok(())
+
+struct History;
+
+impl BuiltinCommand for History {
+    fn name() -> String {
+        String::from("history")
+    }
+
+    fn help() -> String {
+        String::from("\
+history: history [-c] [n]
+    Display the history list with line numbers. Argument of N
+    says to list only the last N lines. The `-c' option causes
+    the history list to be cleared by deleting all of the entries.")
+    }
+
+    fn run(_args: Vec<String>) -> Result<()> {
+        // TODO(rgardner): needs access to the shell. Need to rethink design.
+        Ok(())
+    }
 }
