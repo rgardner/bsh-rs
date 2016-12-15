ec5bb1c747d3abae319d4c3374caa27888aea781
Robert Gardner <bob.hn.gardner@gmail.com>
2016-12-14 23:30:37 -0800

    Add comments and rename info->job in main

--- src/main.rs
+++ src/main.rs
@@ -38,8 +38,8 @@ struct Args {
 
 /// Execute a command string in the context of the shell.
 fn execute_command(mut shell: Shell, command: &str) {
-    let mut info = match ParseJob::parse(command) {
-        Ok(Some(info)) => info,
+    let mut job = match ParseJob::parse(command) {
+        Ok(Some(job)) => job,
         Err(err) => {
             println!("{:?}", err);
             process::exit(1);
@@ -47,7 +47,7 @@ fn execute_command(mut shell: Shell, command: &str) {
         _ => process::exit(1),
     };
 
-    if let Err(e) = shell.run(&mut info) {
+    if let Err(e) = shell.run(&mut job) {
         println!("bsh: {}", e);
         process::exit(1);
     }
@@ -68,7 +68,9 @@ fn main() {
     }
 
     loop {
+        // Check the status of background jobs, removing exited ones.
         shell.check_jobs();
+
         let mut input = String::new();
         match shell.prompt(&mut input) {
             Ok(0) => shell.exit(None),
@@ -78,14 +80,16 @@ fn main() {
 
         input = input.trim().to_owned();
 
+        // Perform history substitutions and add user input to history.
         if let Err(e) = shell.expand_history(&mut input) {
             println!("bsh: {}", e);
             continue;
         }
         shell.add_history(&input);
 
-        let mut info = match ParseJob::parse(&input) {
-            Ok(Some(info)) => info,
+        // Parse user input.
+        let mut job = match ParseJob::parse(&input) {
+            Ok(Some(job)) => job,
             Err(err) => {
                 println!("{:?}", err);
                 continue;
@@ -93,7 +97,7 @@ fn main() {
             _ => continue,
         };
 
-        if let Err(e) = shell.run(&mut info) {
+        if let Err(e) = shell.run(&mut job) {
             println!("bsh: {}", e);
         }
     }
